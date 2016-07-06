2aa60492bd9f0530feec46b6f44fec8eef149aff
Higor Fischer <higorflopes@gmail.com>
2016-07-06 10:43:27 -0300

    Mudanca, funcoes

--- ex/higorf.py
+++ ex/higorf.py
@@ -1,2 +1,11 @@
-def imprimir():
-    print("Frois viadao")
+def hi():
+    return 'hi'
+
+def fatorial(num):
+    i = num - 1
+    while i > 0:
+        num *= i
+        i-=1
+    return num
+
+print(fatorial(0))
