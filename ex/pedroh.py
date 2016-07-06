c3c7170d7d53d4f084d31380c56d5444e6a47f15
Pedro Henrique Lopes de Castro <pedrohlcastro@gmail.com>
2016-07-06 11:15:23 -0300

    test2 pedroh

--- ex/pedroh.py
+++ ex/pedroh.py
@@ -10,7 +10,13 @@ def fatorial(num):
             result *= i
         return result
 
+def ehPar(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     num = int(input())
     print(fatorial(num))
+    print(ehPar(num))
