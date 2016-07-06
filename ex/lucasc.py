422ed4ef89942881d00894624c556c6b4789df93
Lucas Correa <lucashuati@hotmail.com>
2016-07-06 11:23:27 -0300

    update tests

--- ex/lucasc.py
+++ ex/lucasc.py
@@ -3,7 +3,14 @@ def hi():
 
 def fatorial(n):
     a = n;
+    if n == 0:
+        return 1
     while n != 1:
         a *= (n -1)
         n -= 1
     return a
+
+def ehPar(n):
+    if n%2 == 0:
+        return True
+    return False
