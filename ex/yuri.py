adc039fce381d9060f2850489ab1f923a50209eb
Yuri <yuribruno@msn.com>
2016-07-06 11:55:16 -0300

    bosta

--- ex/yuri.py
+++ ex/yuri.py
@@ -6,3 +6,12 @@ def fatorial(n):
       return 1
    else:
       return n*fatorial(n-1)
+
+def ehPar(n):
+    if (n%2) == 0:
+        return True
+    else:
+        return False
+
+def divideString(s):
+    return s.split()
