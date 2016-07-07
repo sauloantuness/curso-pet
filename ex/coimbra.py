c5c7c37fe3b1747a638475d86168f667a95c0c35
Higor Coimbra <higor1308@gmail.com>
2016-07-07 10:23:58 -0300

    Teste_dict

--- ex/coimbra.py
+++ ex/coimbra.py
@@ -28,3 +28,39 @@ def inverso(chave):
 	for letra in lista:
 		chave2 += letra
 	return int(chave2)
+
+def novoDict(nome,matricula,ehMulher):
+	d = {
+		'nome': nome,
+		'matricula' : matricula,
+		'ehMulher' : ehMulher,
+	}
+	return d
+
+def getNome(d1):
+	return d1['nome']
+
+def setNome(d1,nome):
+	d1['nome'] = nome
+	return d1
+
+def setAulas(d2,lista_aulas):
+	d2['aulas'] = lista_aulas
+	return d2
+
+def insereAula(d3,aula):
+	d3['aulas'].append(aula)
+	return d3
+
+def insereAula(d4,aula):
+	if(d4.get('aulas')):
+		d4['aulas'].append(aula)
+	else:
+		d4['aulas'] = [aula]
+	return d4
+
+def getAulas(d):
+	if(d.get('aulas')):
+		return d['aulas']
+	else:
+		return []
