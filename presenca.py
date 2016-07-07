ddedf11fcc191609289058fa97c081e9f324bba2
Pedro Henrique Lopes de Castro <pedrohlcastro@gmail.com>
2016-07-07 10:49:18 -0300

    Presenca pedroh

--- presenca.py
+++ presenca.py
@@ -5,4 +5,10 @@ alunos = [
         'email': 'sauloantuness@gmail.com',
         'telefone': '37-99807-0761'
     },
+    {
+        'nome': 'Pedro Henrique Lopes de Castro',
+        'matricula': 201522040099,
+        'email': 'pedrohlcastro@gmail.com',
+        'telefone': '31-99808-9860'
+    },
 ]
