b2b522eff3d1379d6fa48d3baf3e93d74f4bf51c
Lucas Correa <lucashuati@hotmail.com>
2016-07-07 10:18:59 -0300

    test dict

--- ex/lucasc.py
+++ ex/lucasc.py
@@ -26,3 +26,40 @@ def inverso(ent):
     for i in range(size-1,-1,-1):
         result += string[i]
     return int(result)
+
+def novoDict(nome,matricula,ehMulher):
+    result = {
+        'nome': nome,
+        'matricula': matricula,
+        'ehMulher': ehMulher
+    }
+    return result
+
+def getNome(dictonary):
+    return dictonary['nome']
+
+def setNome(dictonary,nome):
+    dictonary['nome'] = nome
+    return dictonary
+
+def setAulas(dictonary,aulas):
+
+    d = {
+        'aulas': aulas
+    }
+    dictonary.update(d)
+    return dictonary
+
+def insereAula(dictonary,aula):
+    lista = []
+    lista.append(aula)
+    if not(dictonary.get('aulas')):
+        dictonary['aulas'] = lista
+    else:
+        dictonary['aulas'].append(aula)
+    return dictonary
+
+def getAulas(dictonary):
+    if not(dictonary.get('aulas')):
+        return []
+    return dictonary['aulas']
