38352ff0adcbb6bb69fcd6037d47e64427ca08ef
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2016-07-07 10:30:05 -0300

    test_dict

--- ex/gabriela.py
+++ ex/gabriela.py
@@ -18,3 +18,39 @@ def divideString(s):
 def inverso(n):
     s = str(n)
     return int(s[::-1])
+
+def getNome(d):
+    return d.get('nome')
+
+def novoDict(nome,matricula,ehMulher):
+    d = {
+        'nome': nome,
+        'matricula': matricula,
+        'ehMulher': ehMulher,
+    }
+    return d
+
+def setNome(d,nome):
+    d['nome']=nome
+    return d
+
+def setAulas(d,aulas):
+    """novod2 = {
+        'nome': 'Gabriela',
+        'matricula': 2014123456,
+        'ehMulher': False,
+        'aulas': ['Fisica', 'Calculo']
+    }"""
+    d['aulas'] = aulas
+    return d
+
+def insereAula(d,aula):
+    test = d.get("aulas",False)
+    if test == False:
+        d["aulas"] = [aula]
+    else:
+        d["aulas"].append(aula)
+    return d
+
+def getAulas(d):
+    return d.get("aulas",[])
