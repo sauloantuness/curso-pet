864086b3c6c4a85cc94e59247522488f84b81e5e
LucasF <ferreira.alves@live.com>
2016-07-07 10:51:17 -0300

    fix lucasf

--- presenca.py
+++ presenca.py
@@ -5,4 +5,10 @@ alunos = [
         'email': 'sauloantuness@gmail.com',
         'telefone': '37-99807-0761'
     },
+    {
+        'nome': 'Lucas Ferreira Alves',
+        'matricula': 201512040428,
+        'email': 'ferreira.alves@live.com',
+        'telefone': '31-9298-2277'
+    },
 ]
